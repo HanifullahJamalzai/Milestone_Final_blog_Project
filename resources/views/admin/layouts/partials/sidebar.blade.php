bcb04a3780b2f1b4c031ec2a2cffe7cab13379ec
Hanifullah Jamalzai <hanifullahjamalzai2014@gmail.com>
2022-07-18 16:55:45 +0430

    some ...

--- resources/views/admin/layouts/partials/sidebar.blade.php
+++ resources/views/admin/layouts/partials/sidebar.blade.php
@@ -58,6 +58,16 @@
         </a>
       </li><!-- End Dashboard Nav -->
 
+      <li class="nav-heading"></li>
+
+
+      <li class="nav-item">
+        <a class="nav-link " href="{{ route('profile.index')}}">
+          <i class="bi bi-grid text-danger"></i>
+          <span class="text-danger">Logout</span>
+        </a>
+      </li><!-- End Dashboard Nav -->
+
       <li class="nav-item">
         <a class="nav-link collapsed" data-bs-target="#components-nav" data-bs-toggle="collapse" href="#">
           <i class="bi bi-menu-button-wide"></i><span>Components</span><i class="bi bi-chevron-down ms-auto"></i>
